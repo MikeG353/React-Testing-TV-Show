5a83a1400002412784409a41ec6b6eb3756d1612
Michael Gregory <michael.gregory8584@gmail.com>
2020-06-07 20:07:58 -0500

    added comment to remind me where I had an issue changed a test name to reflect mock data use

--- src/APP.test.js
+++ src/APP.test.js
@@ -1,11 +1,19 @@
-import Reacr from 'react'
-import {render} from '@testing-library/react'
+import React from 'react'
+import {render, waitFor, fireEvent} from '@testing-library/react'
 import {fetchShow as mockFetchShow} from "./api/fetchShow"
-import { App } from './App'
+import App from './App'
 import {episodesFixture} from "./components/Episodes.test"
 jest.mock("./api/fetchShow")
-test("App fetches and reders data", () => {
+test("App fetches and reders data", async () => {
+    // was never able to get this to work asyncronously, Need additional help here.
     mockFetchShow.mockResolvedValueOnce(episodesFixture)
+    console.log(mockFetchShow)
+    const { findByText, getByText } = render(<App  />)
+    const fetchingMessage = getByText(/Fetching data.../i)
 
-    render(<App />)
+    await waitFor(() => {
+        const dropdown = findByText(/select a season/i)
+        
+    })
+
 })
